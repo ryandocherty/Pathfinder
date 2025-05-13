0d8a89e4cd366cc2333fa5d1ed9e82b2de5e748d
Ryan Docherty <ryandocherty95@gmail.com>
2025-05-13 11:06:43 +0100

    Changes to be committed: 	new file:   CMP2020M AI A1 - Documentation.pdf 	new file:   Pathfinder.sln 	new file:   Pathfinder/AiBotBase.cs 	new file:   Pathfinder/AiBotRandom.cs 	new file:   Pathfinder/AiBotSimple.cs 	new file:   Pathfinder/Content/0.txt 	new file:   Pathfinder/Content/1.txt 	new file:   Pathfinder/Content/2.txt 	new file:   Pathfinder/Content/3.txt 	new file:   Pathfinder/Content/4.txt 	new file:   Pathfinder/Content/ai.xnb 	new file:   Pathfinder/Content/target.xnb 	new file:   Pathfinder/Content/tile1.xnb 	new file:   Pathfinder/Content/tile2.xnb 	new file:   Pathfinder/Coord2.cs 	new file:   Pathfinder/Game1.cs 	new file:   Pathfinder/Icon.ico 	new file:   Pathfinder/Pathfinder.csproj 	new file:   Pathfinder/Player.cs 	new file:   Pathfinder/Program.cs 	new file:   Pathfinder/Properties/AssemblyInfo.cs 	new file:   Pathfinder/ai.png 	new file:   Pathfinder/bin/Windows/Debug/Content/0.txt 	new file:   Pathfinder/bin/Windows/Debug/Content/1.txt 	new file:   Pathfinder/bin/Windows/Debug/Content/2.txt 	new file:   Pathfinder/bin/Windows/Debug/Content/3.txt 	new file:   Pathfinder/bin/Windows/Debug/Content/4.txt 	new file:   Pathfinder/bin/Windows/Debug/Content/ai.xnb 	new file:   Pathfinder/bin/Windows/Debug/Content/tile1.xnb 	new file:   Pathfinder/bin/Windows/Debug/Content/tile2.xnb 	new file:   Pathfinder/bin/Windows/Debug/Lidgren.Network.dll 	new file:   Pathfinder/bin/Windows/Debug/MonoGame.Framework.dll 	new file:   Pathfinder/bin/Windows/Debug/MonoGame.Framework.xml 	new file:   Pathfinder/bin/Windows/Debug/Pathfinder.exe 	new file:   Pathfinder/bin/Windows/Debug/Pathfinder.pdb 	new file:   Pathfinder/bin/Windows/Debug/Pathfinder.vshost.exe 	new file:   Pathfinder/bin/Windows/Debug/Pathfinder.vshost.exe.manifest 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.DXGI.dll 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.DXGI.xml 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.Direct2D1.dll 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.Direct2D1.xml 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.Direct3D11.dll 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.Direct3D11.xml 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.Direct3D9.dll 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.Direct3D9.xml 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.MediaFoundation.dll 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.MediaFoundation.xml 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.RawInput.dll 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.RawInput.xml 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.XAudio2.dll 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.XAudio2.xml 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.XInput.dll 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.XInput.xml 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.dll 	new file:   Pathfinder/bin/Windows/Debug/SharpDX.xml 	new file:   Pathfinder/level.cs 	new file:   Pathfinder/obj/x86/Debug/DesignTimeResolveAssemblyReferences.cache 	new file:   Pathfinder/obj/x86/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   Pathfinder/obj/x86/Debug/Pathfinder.csproj.FileListAbsolute.txt 	new file:   Pathfinder/obj/x86/Debug/Pathfinder.csprojResolveAssemblyReference.cache 	new file:   Pathfinder/obj/x86/Debug/Pathfinder.exe 	new file:   Pathfinder/obj/x86/Debug/Pathfinder.pdb

---
+++ Pathfinder/AiBotSimple.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using System.IO;
+
+namespace Pathfinder
+{
+    class AiBotSimple : AiBotRandom
+    {
+       public AiBotSimple(int x, int y) : base(x,y)
+        {
+
+        }
+       protected override void ChooseNextGridLocation(Level level, Player plr)
+       {
+           if (plr.GridPosition.X > GridPosition.X)
+           {
+               Coord2 newPos;
+               newPos = GridPosition;
+               
+               
+               if (level.ValidPosition(newPos) == false)
+               {
+                   newPos.X -= 1;
+                   SetNextGridPosition(newPos, level);
+               }
+               else
+               {
+                   newPos.X += 1;
+                   SetNextGridPosition(newPos, level);
+               }
+           }
+
+
+           if (plr.GridPosition.X < GridPosition.X)
+           {
+               Coord2 newPos;
+               newPos = GridPosition;
+
+               if (level.ValidPosition(newPos) == false)
+               {
+                   newPos.X += 1;
+                   SetNextGridPosition(newPos, level);
+               }
+
+               else
+               {
+                   newPos.X -= 1;
+                   SetNextGridPosition(newPos, level);
+               }
+           }
+
+
+           if (plr.GridPosition.Y > GridPosition.Y)
+           {
+               Coord2 newPos;
+               newPos = GridPosition;
+               
+               if (level.ValidPosition(newPos) == false)
+               {
+                   newPos.Y -= 1;
+                   SetNextGridPosition(newPos, level);
+               }
+
+               else
+               {
+                   newPos.Y += 1;
+                   SetNextGridPosition(newPos, level);
+               }
+           }
+
+
+           if (plr.GridPosition.Y < GridPosition.Y)
+           {
+               Coord2 newPos;
+               newPos = GridPosition;
+               
+               if (level.ValidPosition(newPos) == false)
+               {
+                   newPos.Y += 1;
+                   SetNextGridPosition(newPos, level);
+               }
+
+               else
+               {
+                   newPos.Y -= 1;
+                   SetNextGridPosition(newPos, level);
+               }
+           }
+       }
+    }
+}
